629d31cfa69fbbc91fa661a8d18e5a0ac8f2b811
michalsegal11 <michal.segal11@gmail.com>
2024-09-24 14:36:07 +0300

    del put

--- controller/users.js
+++ controller/users.js
@@ -1,4 +1,4 @@
-import {getusers,getUserById,addUser} from '../service/users.js';
+import {getusers,getUserById,addUser,deleteUser,updateUser} from '../service/users.js';
 export class User {
     getAll = async (req, res) => {
 
@@ -15,10 +15,6 @@ export class User {
 
 
     };
-    
-
-   
-
     search = async (req, res) => {
         try {
             const id = req.params.id;
@@ -47,27 +43,11 @@ export class User {
 
         }
     };
-
-    
     delete=( async (req, res) => {
         try {
             const id = req.params.t_z;
-    
-            // כאן תוכל להוסיף ולידציה אם צריך
-    
-            // חיבור למסד נתונים
-            const pool = await sql.connect({
-                user: 'yourUsername',
-                password: 'yourPassword',
-                server: 'yourServer',
-                database: 'yourDatabase'
-            });
-    
-            // ביצוע השאילתא למחיקת המשתמש
-            const result = await pool.request()
-                .input('t_z', sql.Int, id)
-                .query('DELETE FROM users WHERE t_z = @t_z');
-    
+            console.log(`Deleting user with id: ${id}`);
+            const result = await deleteUser(id);
             if (result.rowsAffected[0] === 0) {
                 res.status(404).send("User not found");
             } else {
@@ -79,61 +59,17 @@ export class User {
             res.status(500).send(error.message);
         }
     });
-
-
     put = ( async (req, res) => {
 
         try {
+            const id = req.params.t_z;
             let edituser = req.body;
-            //  add validate
-            const result = await pool.request()
-                .query('SELECT * FROM users');
-            // קבלת תוכן הטבלה
             const users = result.recordset;
-            
-            let sexist = false;
-            for (let index = 0; index < users.length; index++) {
-                if (users[index].t_z== edituser.t_z) {
-                    sexist = true;
-                    for (const key in edituser) {
-                        
-                        users[index][key]  = edituser[key];
-                            
-                    }
-                    break;
-                }
-    
-            }
-            if (!sexist) {
-                res.status(500).send("user not  exist");
-            }
-           // שאילתא לעדכון המשתמש
-           const result1 = await pool.request()
-           .input('t_z', sql.Int, t_z)
-           .input('name', sql.NVarChar, edituser.name) // דוגמה לעדכון שם
-           .input('gmail', sql.NVarChar, edituser.gmail) // דוגמה לעדכון דוא"ל
-           .input('password', sql.Int, edituser.password)
-           .input('adress', sql.NVarChar, edituser.adress) // דוגמה לעדכון שם
-           .input('birthday', sql.NVarChar, edituser.birthday)
-           .input('status', sql.NVarChar, edituser.status)
-    
-           .query('UPDATE users SET name = @name, gmail = @gmail, password = @password,adress = @adress, birthday = @birthday, status = @status WHERE t_z = @t_z');
-    
-       if (result.rowsAffected[0] === 0) {
-           res.status(404).send("User not found");
-       } else {
-           console.log('User updated successfully');
-           res.send({ message: "User updated successfully" });
-       } 
-    } catch (error) {
-        console.log('there was an error:', error.message);
-        res.status(500).send(error.message);
-
-    }
-    
+            let editusers = await updateUser(t_z, edituser); 
+            res.send(editusers);
+        } catch (error) {
+            console.log('There was an error:', error.message);
+            res.status(404).send(error.message);
+        }
     });
-    
-
-
-
 }
