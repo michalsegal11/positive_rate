13e97cf5b1c8fc20974d01ac1a6b56c14c8e23f1
michalsegal11 <michal.segal11@gmail.com>
2024-09-25 17:10:14 +0300

    style..

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const app = express();
 const port = process.env.PORT || 3000
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({ extended: true }));
-app.use('/images',express.static('uploads'))
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use('/images',express.static('uploads'))
 app.get("/users",userRouter);
 app.get("/users/:id",userRouter);
 app.post("/users",userRouter);
